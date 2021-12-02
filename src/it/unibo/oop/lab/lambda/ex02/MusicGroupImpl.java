66a4410b8c60daea8a601ff61751e55d089cffe2
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2021-12-02 22:27:23 +0100

    averageDurationSongs done

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -57,7 +57,8 @@ public final class MusicGroupImpl implements MusicGroup {
 
 	@Override
 	public OptionalDouble averageDurationOfSongs(final String albumName) {
-		return null;
+		return this.songs.stream().filter(s -> s.getAlbumName().isPresent())
+				.filter(s -> s.getAlbumName().get().equals(albumName)).mapToDouble(Song::getDuration).average();
 	}
 
 	@Override
